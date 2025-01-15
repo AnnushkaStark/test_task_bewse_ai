6a13154026b18ef9a160292d1574f113f81ab516
AnnushkaStrak <psk221219@gmail.com>
2025-01-16 01:00:19 +0300

    added base crud mixins amd constants

---
+++ src/crud/crud_mixins/read.py
@@ -0,0 +1,23 @@
+from typing import Generic, Optional, Sequence
+from uuid import UUID
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from constants.crud_types import ModelType
+
+
+class ReadAsync(Generic[ModelType]):
+    async def get_by_id(
+        self, db: AsyncSession, *, obj_id: int
+    ) -> Optional[ModelType]:
+        statement = select(self.model).where(self.model.id == obj_id)
+        result = await db.execute(statement)
+        return result.scalars().first()
+
+    async def get_by_uid(
+        self, db: AsyncSession, *, uid: UUID
+    ) -> Optional[ModelType]:
+        statement = select(self.model).where(self.model.uid == uid)
+        result = await db.execute(statement)
+        return result.scalars().first()

2638021d325faa3920108bff01e2d7f9583bb9c6
AnnushkaStrak <psk221219@gmail.com>
2025-01-16 18:50:25 +0300

    added all services

---
+++ src/services/application.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from crud.application import application_crud
+from models import Application, User
+from schemas.application import ApplicationCreate, ApplicationCreateDB
+from services import broker as broker_service
+
+
+async def create(
+    db: AsyncSession, create_schema: ApplicationCreate, author: User
+) -> Application:
+    create_data = ApplicationCreateDB(
+        author_id=author.id, **create_schema.model_dump(exclude_unset=True)
+    )
+    application = await application_crud.create(
+        db=db, create_schema=create_data
+    )
+    await broker_service.send_message(author=author, application=application)
+    return application
---
+++ src/services/broker.py
@@ -0,0 +1,13 @@
+from models import Application, User
+from schemas.kafka_message import KafkaMessageBase
+
+from ..broker import send_application
+
+
+async def send_message(author: User, application: Application) -> None:
+    message_schema = KafkaMessageBase(
+        username=author.username,
+        description=application.description,
+        created_at=application.created_at,
+    )
+    await send_application(application_data=message_schema.model_dump())
